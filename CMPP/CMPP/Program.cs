739490c8e825ed96aa10f38343506acde3e7defb
姬逍遥 <166687143@qq.com>
2017-11-12 15:56:52 +0800

    创建

---
+++ CMPP/CMPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
