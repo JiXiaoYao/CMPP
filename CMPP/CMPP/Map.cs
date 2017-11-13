73ffd007b69b18c9465221b2972e08eaf68b231a
姬逍遥 <1666871743@qq.com>
2017-11-13 18:06:16 +0800

    启动项目

---
+++ CMPP/CMPP/Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TCPCS;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CMPP
+{
+    class Map
+    {
+        Server TcpServer;
+       
+    }
+    enum MapNetMode
+    {
+        TCP, UDP, TCPUDP
+    }
+}
