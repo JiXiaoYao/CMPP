e10a9a9d4d7c2e64184e18460b5f0e24c2d154bb
姬逍遥 <1666871743@qq.com>
2017-11-13 19:06:02 +0800

    写了一部分

--- CMPP/CMPP/Control.cs
+++ CMPP/CMPP/Control.cs
@@ -1,11 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using TCPCS;
 
 namespace CMPP
 {
-    class Control
+    public class Control
     {
+        TCPCS.Server TcpServer;
+        Dictionary<string, string> HaveLogin = new Dictionary<string, string>();                   // kyes:User     Value:IP:Port&SocketID
+        Dictionary<IPEndPoint, long> ConnetListDict = new Dictionary<IPEndPoint, long>();
+        string MessageBufferZone;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="ListenIP"></param>
+        /// <param name="ListenPort"></param>
+        public Control(IPAddress ListenIP, int ListenPort)
+        {
+            TcpServer = new TCPCS.Server(new IPAddress[] { ListenIP }, ListenPort, 4096, true);
+            TcpServer.StartListen();
+            TcpServer.ClientConnent += NewConnet;
+            TcpServer.ClientMessages += ClientMessage;
+            TcpServer.ConnentStop += ConnetStop;
+        }
+        #region 通信管道处理代码
+        public void NewConnet(IPEndPoint ListenEndPoint, long SocketID)
+        {
+            Thread.Sleep(1000 * 60);
+            if (HaveLogin.ContainsValue(ListenEndPoint.ToString() + "&" + SocketID))
+            {
 
+            }
+            else
+            {
+                if (TcpServer.SocketIPEndPointDict[ListenEndPoint].ContainsKey(SocketID))
+                {
+                    TcpServer.Send(ListenEndPoint, (int)SocketID, Encoding.UTF8.GetBytes(Packet.Encapsulation(Encoding.UTF8.GetBytes("Time Out"))));
+                    TcpServer.ConnentStop(ListenEndPoint, SocketID);
+                }
+            }
+        }
+        public void ClientMessage(IPEndPoint Listen, long SocketID, byte[] Context, int Legth)
+        {
+            string ContextMessage = Encoding.UTF8.GetString(Context,0,Legth);
+            MessageBufferZone = MessageBufferZone + Context;
+            if (MessageBufferZone.Contains("<DataStart>") && MessageBufferZone.Contains("<DataEnd>"))
+            {
+
+            }
+        }
+        public void ConnetStop(IPEndPoint Listen, long SocketID)
+        {
+
+        }
+        #endregion
     }
 }
--- CMPP/CMPP/Map.cs
+++ CMPP/CMPP/Map.cs
@@ -11,7 +11,42 @@ namespace CMPP
     class Map
     {
         Server TcpServer;
-       
+        Dictionary<IPEndPoint, string> MapListDict = new Dictionary<IPEndPoint, string>();
+        
+        public delegate bool SelectUserAndPasswordDelegate(string User, string Password);
+        public SelectUserAndPasswordDelegate SelectUserAndPassword;
+        #region 构造函数    
+        public Map(IPAddress Communication, int Port)
+        {
+            TcpServer = new Server(new IPAddress[] { Communication }, Port, 4096, true);
+            TcpServer.StartListen();
+            TcpServer.ClientConnent += NewConnet;
+            TcpServer.ClientMessages += ClientMessage;
+            TcpServer.ConnentStop += ConnetStop;
+        }
+        #endregion
+        #region 通信管道处理代码
+        public void NewConnet(IPEndPoint Listen, long SocketID)
+        {
+
+        }
+        public void ClientMessage(IPEndPoint Listen, long SocketID, byte[] Context, int Legth)
+        {
+
+        }
+        public void ConnetStop(IPEndPoint Listen, long SocketID)
+        {
+
+        }
+        #endregion
+
+        public void AddMap(IPEndPoint ListenEndPoint, string User, MapNetMode Mode)
+        {
+            if (Mode == MapNetMode.TCP)
+            {
+                MapListDict.Add(ListenEndPoint, User);
+            }
+        }
     }
     enum MapNetMode
     {
